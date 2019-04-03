4ec6887c1c5583b3a51a17e4b78a6a95d44d9575
rj <>
2019-04-03 16:18:38 -0400

    Improved testing of parsing for gcov.

--- gcov_test.go
+++ gcov_test.go
@@ -8,13 +8,17 @@ import (
 func TestParseFunctionRecord(t *testing.T) {
 	cases := []struct {
 		value    string
+		ok       bool
 		name     string
 		hitCount uint64
 	}{
-		{"function:222,6,main", "main", 6},
-		{"function:222,223,6,main", "main", 6},
-		{"function:222,0,main", "main", 0},
-		{"function:222,223,0,main", "main", 0},
+		{"function:222,6,main", true, "main", 6},
+		{"function:222,223,6,main", true, "main", 6},
+		{"function:222,0,main", true, "main", 0},
+		{"function:222,223,0,main", true, "main", 0},
+		{"function:222,#,main", false, "", 0},
+		{"function:222,223,#,main", false, "", 0},
+		{"function:222,main", false, "", 0},
 	}
 
 	for _, v := range cases {
@@ -30,8 +34,11 @@ func TestParseFunctionRecord(t *testing.T) {
 			if hc != v.hitCount {
 				LogNE(t, "hit count", v.hitCount, hc)
 			}
-			if err != nil {
-				LogNE(t, "error", nil, err)
+			if (err == nil) != v.ok {
+				LogNE(t, "ok", v.ok, err == nil)
+				if err != nil {
+					t.Logf("err = %s", err)
+				}
 			}
 		})
 	}
@@ -40,11 +47,17 @@ func TestParseFunctionRecord(t *testing.T) {
 func TestParseLCountRecord(t *testing.T) {
 	cases := []struct {
 		value    string
+		ok       bool
 		lineNo   int
 		hitCount uint64
 	}{
-		{"lcount:32,18", 32, 18},
-		{"lcount:32,0", 32, 0},
+		{"lcount:32,18", true, 32, 18},
+		{"lcount:32,0", true, 32, 0},
+		{"lcount:32,18,0", true, 32, 18},
+		{"lcount:32,0,1", true, 32, 0},
+		{"lcount:32,#", false, 0, 0},
+		{"lcount:#,18", false, 0, 0},
+		{"lcount:32", false, 0, 0},
 	}
 
 	for _, v := range cases {
@@ -60,8 +73,48 @@ func TestParseLCountRecord(t *testing.T) {
 			if hc != v.hitCount {
 				LogNE(t, "hit count", v.hitCount, hc)
 			}
-			if err != nil {
-				LogNE(t, "error", nil, err)
+			if (err == nil) != v.ok {
+				LogNE(t, "ok", v.ok, err == nil)
+				if err != nil {
+					t.Logf("err = %s", err)
+				}
+			}
+		})
+	}
+}
+
+func TestParseBranchRecord(t *testing.T) {
+	cases := []struct {
+		value  string
+		ok     bool
+		lineNo int
+		status BranchStatus
+	}{
+		{"branch:176,taken", true, 176, BranchTaken},
+		{"branch:176,nottaken", true, 176, BranchNotTaken},
+		{"branch:176,notexec", true, 176, BranchNotExec},
+		{"branch:176", false, 0, 0},
+		{"branch:#,taken", false, 0, 0},
+	}
+
+	for _, v := range cases {
+		t.Run(v.value, func(t *testing.T) {
+			rt, value := recordType(v.value)
+			if rt != "branch" {
+				LogNE(t, "record type", "lcount", rt)
+			}
+			lineNo, status, err := parseBranchRecord(value)
+			if lineNo != v.lineNo {
+				LogNE(t, "function name", v.lineNo, lineNo)
+			}
+			if status != v.status {
+				LogNE(t, "hit count", v.status, status)
+			}
+			if (err == nil) != v.ok {
+				LogNE(t, "ok", v.ok, err == nil)
+				if err != nil {
+					t.Logf("err = %s", err)
+				}
 			}
 		})
 	}
