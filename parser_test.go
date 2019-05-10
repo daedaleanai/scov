9ea17266e67f92c590061cc51526596ef53c845b
rj <>
2019-05-10 14:46:00 -0400

    Added test cases to cover loading data from a directory.

--- parser_test.go
+++ parser_test.go
@@ -42,13 +42,20 @@ func TestParserLoadFile(t *testing.T) {
 		fcov      Coverage
 		bcov      Coverage
 	}{
+		// gcc 7.4.0
 		{"example-7.4.0.c.gcov", Coverage{9, 10}, Coverage{9, 10}, Coverage{1, 1}, Coverage{}},
 		{"example-7.4.0-branches.c.gcov", Coverage{9, 10}, Coverage{9, 10}, Coverage{1, 1}, Coverage{2, 4}},
+		{"example-7.4.0-branches", Coverage{18, 22}, Coverage{9, 10}, Coverage{1, 1}, Coverage{2, 4}},
+		// gcc 8.3.0
 		{"example-8.3.0.c.gcov", Coverage{9, 10}, Coverage{9, 10}, Coverage{1, 1}, Coverage{}},
 		{"example-8.3.0-branches.c.gcov", Coverage{9, 10}, Coverage{9, 10}, Coverage{1, 1}, Coverage{2, 4}},
-		{"example-lcov-1.13.info", Coverage{18, 22}, Coverage{9, 10}, Coverage{1, 1}, Coverage{2, 4}},
-		{"example-llvm-8.0.1.info", Coverage{57, 67}, Coverage{28, 31}, Coverage{3, 3}, Coverage{0, 0}},
+		{"example-8.3.0-branches", Coverage{18, 22}, Coverage{9, 10}, Coverage{1, 1}, Coverage{2, 4}},
+		// gcc 9.1.0
 		{"example-9.1.0.c.gcov.json.gz", Coverage{9, 10}, Coverage{9, 10}, Coverage{1, 1}, Coverage{0, 0}},
+		// gcc with lcov
+		{"example-lcov-1.13.info", Coverage{18, 22}, Coverage{9, 10}, Coverage{1, 1}, Coverage{2, 4}},
+		// clang 8.0.1
+		{"example-llvm-8.0.1.info", Coverage{57, 67}, Coverage{28, 31}, Coverage{3, 3}, Coverage{0, 0}},
 	}
 	for _, v := range cases {
 		t.Run(v.filename, func(t *testing.T) {
