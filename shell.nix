9b4bc0f29e6d0a2a899fb87c76afffcc2c3bd641
rj <>
2021-05-30 22:57:51 -0400

    Replace uses of nix-env in scripts with packages

--- shell.nix
+++ shell.nix
@@ -1,8 +1,9 @@
-{ pkgs ? import <nixpkgs> {} }:
+{ pkgs ? import <nixpkgs> { }, enableLint ? false }:
 
 pkgs.mkShell {
   # No dependencies beyond stdlib.
-  buildInputs = [ pkgs.go pkgs.gitMinimal ];
+  buildInputs = [ pkgs.go pkgs.gitMinimal ]
+    ++ (if enableLint then [ pkgs.golangci-lint ] else [ ]);
 
   # Make sure we don't pick up the users' GOPATH.
   # Advertise the current version of go when shell starts.
